6e9aaf2ccf3e61ed67044b42be82ca613289822e
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-01-18 00:58:48 +0000

    Unit test

---
+++ service/src/test/kotlin/com/integreety/activityengine/service/ActivityServiceShould.kt
@@ -0,0 +1,14 @@
+package com.integreety.activityengine.service
+
+import org.junit.jupiter.api.Test
+
+internal class ActivityServiceShould {
+
+    @Test
+    fun find() {
+    }
+
+    @Test
+    fun save() {
+    }
+}
